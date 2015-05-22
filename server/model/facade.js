e1cc15410df43cdbe1920acd2ebd566f6df046ed
fisker23 <fisker23@hotmail.com>
2015-05-22 12:56:55 +0200

    Hej 123

--- server/model/facade.js
+++ server/model/facade.js
@@ -7,6 +7,7 @@ var request = require('request');
 var User = mongoose.model("User");
 var airline = mongoose.model("Airline");
 var bodyParser = require('body-parser');
+var async = require('async');
 
 //var bob = new airline({URLtosite: 'http://semesterproject-testnikolai1.rhcloud.com/SemesterProjectFligths/', name: 'Gruppe03'});
 //bob.save(function(err) {
@@ -45,13 +46,15 @@ function _addUser(uName, uEmail, uPw){
 }
 
 function getAllAvailableFlights(callback) {
-
+var airtemp = [];
     airline.find({}, function(err, result){
 
         result.forEach(function(data){
 
-            request(data.URLtosite+'api/flights', function(err, data){
-                callback(null, data);
+            request(data.URLtosite+'api/flights/BER/1432283031', function(err,res,body){
+              airtemp.push(body)
+                console.log(body)
+                callback(null, body);
             })
 
         })
@@ -68,18 +71,60 @@ function _checkUser(uName,uPw,callback){
    // console.log(User.find(usertoCheck));
 }
 
-//_addUser("Lars","HAADASDJOKL","test")
-//_addUser("Allan","HASSAN@cphbusiness.dk","test")
-//_addUser("HADI ABDUL","SKINKE@cphbusiness.dk","test")
-//
-//getAllAvailableFlights(function(item){
-//
-//});
-//_checkUser("henning","kage");
+var performSearch = function (startAirport, startDate ,callback){
 
+    airline.find({}, function(err, data){
+        if(err){return callback(err);}
+
+        var tasks = [];
+
+        for(var i = 0; i < data.length; i++){
+            tasks.push(getRequest({
+                url: data[i].URLtosite+"api/flights/"+startAirport+"/"+startDate,
+                method: 'GET',
+                json: true
+            }));
+        };
+
+        async.parallel(tasks,function(err, result){
+            if(err) return console.log(err);
+
+            return callback(null, result);
+        });
+
+
+    });
+};
+
+var getRequest = function (elem){
+    return (function(callback)
+    {
+        request(elem, function (error, response, body) {
+            if (!error) {
+                callback(null, body);
+            }
+            else {
+                callback(error);
+            }
+        });
+    });
+};
+
+
+//performSearch("CPH",1430784000000,function(err,res){
+////console.log("data", res)
+//    res.forEach(function(datan){
+//
+//        if(datan instanceof Array){
+//            console.log(datan)
+//        }})
+//})
 module.exports = {
+    performSearch: performSearch,
     addUser: _addUser,
     checkUser: _checkUser,
     getAllAvailableFlights: getAllAvailableFlights
 
 }
+
+
--- server/routes/REST_Users_API.js
+++ server/routes/REST_Users_API.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var facade = require('../model/facade');
 
 var router = express.Router();
 router.get('/test', function(req, res) {
@@ -12,6 +13,19 @@ router.get('/partials/:partialName', function(req, res) {
     var name = req.params.partialName;
     res.render('partials/' + name);
 });
+
+router.get('/flights/:air/:time',function(req,res){
+    var air = req.params.air;
+    var time = req.params.time;
+    facade.performSearch(air,time,function(err,result){
+
+        result.forEach(function(datan){
+
+            if(datan instanceof Array){
+                res.send(datan)
+            }})
+    })
+})
 
 router.get('/getreservation', function(req,res){
 
